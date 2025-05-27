a502501e27f2a2e1f0f368961aa12dfb8997333d
Filip Wasil <f.wasil@samsung.com>
2025-05-27 13:09:24 +0200

    riscv detection

--- simd/rvv/jsimd.c
+++ simd/rvv/jsimd.c
@@ -13,68 +13,8 @@
 static unsigned int simd_support = ~0;
 
 #if defined(__linux__)
-
-#define SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT  (1024 * 1024)
-/* TODO: Check rvv intrinsic support by examine /proc/cpuinfo */
-
-// LOCAL(int)
-// check_feature(char *buffer, char *feature)
-// {
-//   char *p;
-
-//   if (*feature == 0)
-//     return 0;
-//   if (strncmp(buffer, "ASEs implemented", 16) != 0)
-//     return 0;
-//   buffer += 16;
-//   while (isspace(*buffer))
-//     buffer++;
-
-//   /* Check if 'feature' is present in the buffer as a separate word */
-//   while ((p = strstr(buffer, feature))) {
-//     if (p > buffer && !isspace(*(p - 1))) {
-//       buffer++;
-//       continue;
-//     }
-//     p += strlen(feature);
-//     if (*p != 0 && !isspace(*p)) {
-//       buffer++;
-//       continue;
-//     }
-//     return 1;
-//   }
-//   return 0;
-// }
-
-// LOCAL(int)
-// parse_proc_cpuinfo(int bufsize)
-// {
-//   char *buffer = (char *)malloc(bufsize);
-//   FILE *fd;
-
-//   simd_support = 0;
-
-//   if (!buffer)
-//     return 0;
-
-//   fd = fopen("/proc/cpuinfo", "r");
-//   if (fd) {
-//     while (fgets(buffer, bufsize, fd)) {
-//       if (!strchr(buffer, '\n') && !feof(fd)) {
-//         /* "impossible" happened - insufficient size of the buffer! */
-//         fclose(fd);
-//         free(buffer);
-//         return 0;
-//       }
-//       if (check_feature(buffer, "loongson-rvv"))
-//         simd_support |= JSIMD_RVV;
-//     }
-//     fclose(fd);
-//   }
-//   free(buffer);
-//   return 1;
-// }
-
+#include <asm/hwcap.h>
+#include <sys/auxv.h>
 #endif
 
 /* RVV SIMD support is always available during test. */
@@ -89,27 +29,15 @@ init_simd(void)
 #ifndef NO_GETENV
   char *env = NULL;
 #endif
-#if defined(__linux__)
-  int bufsize = 1024; /* an initial guess for the line buffer size limit */
-#endif
 
   if (simd_support != ~0U)
     return;
 
   simd_support = 0;
-  simd_support |= JSIMD_RVV;
+#if defined(__linux__)
+  simd_support = getauxval (AT_HWCAP) & COMPAT_HWCAP_ISA_V ? JSIMD_RVV : 0;
+#endif
 
-// #if defined(__linux__)
-//   while (!parse_proc_cpuinfo(bufsize)) {
-//     bufsize *= 2;
-//     if (bufsize > SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT)
-//       break;
-//   }
-// #elif defined(__mips_loongson_vector_rev)
-//   /* Only enable rvv by default on non-Linux platforms when the compiler flags
-//    * support it. */
-//   simd_support |= JSIMD_RVV;
-// #endif
 
 #ifndef NO_GETENV
   /* Force different settings through environment variables */
