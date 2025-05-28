65d67307aa316c10065397cb2879109af8e2b7a8
Filip Wasil <f.wasil@samsung.com>
2025-05-28 10:31:35 +0200

    Fix RISC-V RVV refactor

--- simd/rvv/jsimd.c
+++ simd/rvv/jsimd.c
@@ -1,9 +1,35 @@
+/*
+ * Risc-V vector extension optimizations for libjpeg-turbo
+ *
+ * Copyright (C) 2014, D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2025, Samsung. All Rights Reserved.
+ *
+ * Authors:  Filip Wasil     <f.wasil@samsung.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+
 #define JPEG_INTERNALS
-#include "../../jinclude.h"
-#include "../../jpeglib.h"
-#include "../../jsimd.h"
-#include "../../jdct.h"
-#include "../../jsimddct.h"
+#include "jinclude.h"
+#include "jpeglib.h"
+#include "jsimd.h"
+#include "jdct.h"
+#include "jsimddct.h"
 #include "../jsimd.h"
 
 #include <stdio.h>
@@ -13,68 +39,8 @@
 static unsigned int simd_support = ~0;
 
 #if defined(__linux__)
-
-#define SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT  (1024 * 1024)
-/* TODO: Check rvv intrinsic support by examine /proc/cpuinfo */
-
-// LOCAL(int)
-// check_feature(char *buffer, char *feature)
-// {
-//   char *p;
-
-//   if (*feature == 0)
-//     return 0;
-//   if (strncmp(buffer, "ASEs implemented", 16) != 0)
-//     return 0;
-//   buffer += 16;
-//   while (isspace(*buffer))
-//     buffer++;
-
-//   /* Check if 'feature' is present in the buffer as a separate word */
-//   while ((p = strstr(buffer, feature))) {
-//     if (p > buffer && !isspace(*(p - 1))) {
-//       buffer++;
-//       continue;
-//     }
-//     p += strlen(feature);
-//     if (*p != 0 && !isspace(*p)) {
-//       buffer++;
-//       continue;
-//     }
-//     return 1;
-//   }
-//   return 0;
-// }
-
-// LOCAL(int)
-// parse_proc_cpuinfo(int bufsize)
-// {
-//   char *buffer = (char *)malloc(bufsize);
-//   FILE *fd;
-
-//   simd_support = 0;
-
-//   if (!buffer)
-//     return 0;
-
-//   fd = fopen("/proc/cpuinfo", "r");
-//   if (fd) {
-//     while (fgets(buffer, bufsize, fd)) {
-//       if (!strchr(buffer, '\n') && !feof(fd)) {
-//         /* "impossible" happened - insufficient size of the buffer! */
-//         fclose(fd);
-//         free(buffer);
-//         return 0;
-//       }
-//       if (check_feature(buffer, "loongson-rvv"))
-//         simd_support |= JSIMD_RVV;
-//     }
-//     fclose(fd);
-//   }
-//   free(buffer);
-//   return 1;
-// }
-
+#include <asm/hwcap.h>
+#include <sys/auxv.h>
 #endif
 
 /* RVV SIMD support is always available during test. */
@@ -89,27 +55,15 @@ init_simd(void)
 #ifndef NO_GETENV
   char *env = NULL;
 #endif
-#if defined(__linux__)
-  int bufsize = 1024; /* an initial guess for the line buffer size limit */
-#endif
 
   if (simd_support != ~0U)
     return;
 
   simd_support = 0;
-  simd_support |= JSIMD_RVV;
+#if defined(__linux__)
+  simd_support = getauxval (AT_HWCAP) & COMPAT_HWCAP_ISA_V ? JSIMD_RVV : 0;
+#endif
 
-// #if defined(__linux__)
-//   while (!parse_proc_cpuinfo(bufsize)) {
-//     bufsize *= 2;
-//     if (bufsize > SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT)
-//       break;
-//   }
-// #elif defined(__mips_loongson_vector_rev)
-//   /* Only enable rvv by default on non-Linux platforms when the compiler flags
-//    * support it. */
-//   simd_support |= JSIMD_RVV;
-// #endif
 
 #ifndef NO_GETENV
   /* Force different settings through environment variables */
